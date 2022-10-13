df215db5e288c6a08b8d6add0f38b903fcb74e7f
Mohamed Alaa <30278308+mohamed3laa33@users.noreply.github.com>
2022-10-13 15:45:29 +0400

    Create deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+read -p "Enter Your fee recipient/Ethereum/Metamask address: " code
+[[ "$code" =~ ^0x[0-9a-fA-F]{40}$ ]] && echo "" || { echo "You have entered an invalid address"; exit $ERRCODE; }
+sudo echo "recipientcode=$code" > /var/tmp/.recipientcode.txt
+#sudo sed -i  -e "s/recipientcode/$code/g" /var/tmp/docker-compose.yaml
+jwtDir="JWT"
+jwtFile="jwt.hex"
+if [ $# -eq 0 ]; then
+      read -p "Enter Your JWT token: " token
+      echo $token > "$jwtFile"
+fi
+while test $# != 0
+do
+    case "$1" in
+    -j|--jwt)
+      if [ -d "$jwtDir" ]; then sudo rm -Rf $jwtDir/; fi
+      echo "generating hex file"
+      openssl rand -hex 32 | tr -d "\n" > "$jwtFile"
+      ;;
+    *) 
+      read -p "Enter Your JWT token: " token                               
+      echo $token > "$jwtFile"
+      ;;
+    esac
+    shift
+done
+
+mkdir -p $jwtDir
+sudo mv jwt.hex $jwtDir/
+sudo systemctl restart beacon-geth
