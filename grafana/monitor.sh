69e3d325758faf9710028fd02277e480c2ed6dd7
Mohamed Alaa <30278308+mohamed3laa33@users.noreply.github.com>
2022-12-23 11:49:19 +0200

    Update monitor.sh

--- grafana/monitor.sh
+++ grafana/monitor.sh
@@ -6,6 +6,7 @@ prometheusServicePath=https://raw.githubusercontent.com/yashLN/Beacon_Presync/gr
 prometheus_datasource=https://raw.githubusercontent.com/yashLN/Beacon_Presync/grafana/grafana/prometheus_datasource.yaml
 dashboard=https://raw.githubusercontent.com/yashLN/Beacon_Presync/grafana/grafana/dashboard.yaml
 small_amount_validators=https://docs.prylabs.network/assets/grafana-dashboards/small_amount_validators.json
+node_exporter=https://raw.githubusercontent.com/yashLN/Beacon_Presync/grafana/grafana/node_exporter.service
 
 echo "###### Downloading Prometheus Tar File ######"
 sudo mkdir -p /etc/prometheus
@@ -14,6 +15,8 @@ sudo mkdir -p /var/lib/prometheus
 wget https://github.com/prometheus/prometheus/releases/download/v2.41.0-rc.0/prometheus-2.41.0-rc.0.linux-amd64.tar.gz
 tar -xvf prometheus-2.41.0-rc.0.linux-amd64.tar.gz
 cd prometheus-2.41.0-rc.0.linux-amd64
+
+
 
 sudo cp -n -r  prometheus promtool /usr/local/bin/
 sudo cp -n -r consoles/ console_libraries/ /etc/prometheus/
@@ -32,9 +35,28 @@ echo "Installed Prometheus Successfully"
 echo "Adding Prometheus Service"
 curl -o prometheus.service $prometheusServicePath 
 sudo cp -n prometheus.service /etc/systemd/system/prometheus.service
-sudo systemctl start prometheus
+
+
+echo "###########################################################################"
+echo "####################### Installing Node Exporter ##########################"
+echo "###########################################################################"
+
+wget https://github.com/prometheus/node_exporter/releases/download/v1.5.0/node_exporter-1.5.0.linux-amd64.tar.gz
+tar -xzvf node_exporter-1.5.0.linux-amd64.tar.gz
+sudo useradd -rs /bin/false nodeusr || true 
+sudo cp -r -n node_exporter-1.5.0.linux-amd64.tar.gz/node_exporter /usr/local/bin/
+curl -o node_exporter.service $node_exporter
+sudo systemctl daemon-reload
+sudo systemctl enable node_exporter
+sudo systemctl restart node_exporter
+sudo systemctl restart prometheus
 sudo systemctl enable prometheus
 sudo systemctl status prometheus
+
+echo "###########################################################################"
+echo "#################### Installing Grafana ###################################"
+echo "###########################################################################"
+
 curl -o grafana-enterprise-9.3.2-1.x86_64.rpm https://dl.grafana.com/enterprise/release/grafana-enterprise-9.3.2-1.x86_64.rpm
 sudo yum install grafana-enterprise-9.3.2-1.x86_64.rpm -y || true 
 
