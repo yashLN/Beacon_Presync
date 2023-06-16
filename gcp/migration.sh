e48f3d287fc34496b7ea6a2818cd4364309200d4
Mohamed Alaa <30278308+mohamed3laa33@users.noreply.github.com>
2023-06-16 18:12:38 +0300

    Create migration.sh

---
+++ gcp/migration.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+set -e
+color ()
+{
+  echo -en "\033[31m"  ## red
+  eval $* | while read line; do
+      echo -en "\033[36m"  ## blue
+      echo $line
+      echo -en "\033[31m"  ## red
+  done
+  echo -en "\033[0m"
+}
+exec 3>&1
+log ()
+{
+    color echo "INFO: $1" 1>&3
+}
+log "Download Dockercompose and Beacon services"
+
+wget https://raw.githubusercontent.com/yashLN/Beacon_Presync/main/gcp/docker-compose.yaml
+wget https://raw.githubusercontent.com/yashLN/Beacon_Presync/main/gcp/beacon-geth.service
+
+log "Downloaded!!"
+log "Stopping the current beacon-geth service"
+sudo systemctl stop beacon-geth.service
+[ -e /var/docker-compose.yaml ] && sudo rm /var/docker-compose.yaml
+log "Stopped beacon-geth Service"
+
+log "Apply the new beacon-geth Service"
+
+sudo cp docker-compose.yaml /var/docker-compose.yaml
+sudo cp beacon-geth.service  /etc/systemd/system/beacon-geth.service
+
+sudo systemctl daemon-reload
+echo "#######################################################################################"
+sleep 2
+
+log "Starting the new service"
+docker pull public.ecr.aws/n2u0q7l0/beacon:stable
+docker pull public.ecr.aws/n2u0q7l0/geth:stable
+
+sudo systemctl start beacon-geth.service
+
+log "Cleaning Up Everything"
+sudo rm -rf docker-compose-grpc.yaml beacon-geth.service
